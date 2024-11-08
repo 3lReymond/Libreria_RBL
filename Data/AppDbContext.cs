225abd135e6bc9708088f038346e7b02c7ec3dbb
3lreymon <ramonbatidas@gmail.com>
2024-11-08 14:30:19 -0700

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using libreria_RBL.Data.Models;
+using libreria_RBL.Data.Models;
+using System.Collections.Generic;
+
+namespace libreriaa_MALH.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
